4a63d7240d429573f603c4458813f5b7ab798a83
iyuangang <yuangang@me.com>
2017-03-21 14:16:13 +0800

    Revert "VIM Plugin bug"
    This reverts commit d83cf265547af6ca312a48b6cbaaf017bb66e6d4.
    
    Changes to be committed:
    	modified:   controller/admin.class.php
    	modified:   model/admin.model.php
    	modified:   readme.md
    	modified:   view/admin/sal.html
    
     Untracked files:
    	.gitignore

--- model/admin.model.php
+++ model/admin.model.php
@@ -2,192 +2,192 @@
 
 class admin extends medoo
 {
-    public function cat_list()
-    {
-        $datas = $this->select("shop_cat", [
-            "id",
-            "catname"
-        ], [
-            "ORDER" => "id"
-        ]);
+	public function cat_list()
+	{
+		$datas = $this->select("shop_cat", [
+			"id",
+			"catname"
+		], [
+			"ORDER" => "id"
+		]);
 
-        return $datas;
-    }
+		return $datas;
+	}
 
-    public function cat_add( $catname )
-    {
-        $last_cat_id = $this->insert("shop_cat", [
-            "catname" => $catname
-        ]);
+	public function cat_add( $catname )
+	{
+		$last_cat_id = $this->insert("shop_cat", [
+			"catname" => $catname
+		]);
 
-        return $last_cat_id;
-    }
+		return $last_cat_id;
+	}
 
-    public function cat_update( $cat )
-    {
-        $update_num = $this->update("shop_cat", [
-            "catname" => $cat['catname']
-        ], [
-            "id[=]" => $cat['id']
-        ]);
+	public function cat_update( $cat )
+	{
+		$update_num = $this->update("shop_cat", [
+			"catname" => $cat['catname']
+		], [
+			"id[=]" => $cat['id']
+		]);
 
-        return $update_num;
-    }
+		return $update_num;
+	}
 
-    public function data_delete( $table , $id )
-    {
-        $delete_num = $this->delete($table, [
-            "id[=]" => $id
-        ]);
+	public function data_delete( $table , $id )
+	{
+		$delete_num = $this->delete($table, [
+			"id[=]" => $id
+		]);
 
-        return $delete_num;
-    }
+		return $delete_num;
+	}
 
-    public function content_list( $deadline )
-    {
-        $datas = $this->select("shop_content", [
-            "id",
-            "title",
-            "cat",
-            "date",
-            "picture",
-            "url"
-        ], [
-            "date[>]" => $deadline,
-            "ORDER" => "date DESC",
-            "ORDER" => "id DESC"
-        ]);
+	public function content_list( $deadline )
+	{
+		$datas = $this->select("shop_content", [
+			"id",
+			"title",
+			"cat",
+			"date",
+			"picture",
+			"url"
+		], [
+			"date[>]" => $deadline,
+			"ORDER" => "date DESC",
+			"ORDER" => "id DESC"
+		]);
 
-        return $datas;
-    }
+		return $datas;
+	}
 
-    public function dead_list( $deadline )
-    {
-        $datas = $this->select("shop_content", [
-            "id",
-            "title",
-            "cat",
-            "date",
-            "picture",
-            "url"
-        ], [
-            "date[<=]" => $deadline,
-            "ORDER" => "date DESC"
-        ]);
+	public function dead_list( $deadline )
+	{
+		$datas = $this->select("shop_content", [
+			"id",
+			"title",
+			"cat",
+			"date",
+			"picture",
+			"url"
+		], [
+			"date[<=]" => $deadline,
+			"ORDER" => "date DESC"
+		]);
 
-        return $datas;
-    }
+		return $datas;
+	}
 
-    public function content_add( $content )
-    {
-        $last_content_id = $this->insert("shop_content", [
-            "title" => $content['title'],
-            "cat" => $content['cat'],
-            "date" => $content['date'],
-            "picture" => $content['picture'],
-            "url" => $content['url'],
-        ]);
+	public function content_add( $content )
+	{
+		$last_content_id = $this->insert("shop_content", [
+			"title" => $content['title'],
+			"cat" => $content['cat'],
+			"date" => $content['date'],
+			"picture" => $content['picture'],
+			"url" => $content['url'],
+		]);
 
-        return $last_content_id;
-    }
+		return $last_content_id;
+	}
 
-    public function get_content_by_id( $id )
-    {
-        $datas = $this->select("shop_content", [
-            "id",
-            "title",
-            "cat",
-            "date",
-            "picture",
-            "url"
-        ], [
-            "id[=]" => $id
-        ]);
+	public function get_content_by_id( $id )
+	{
+		$datas = $this->select("shop_content", [
+			"id",
+			"title",
+			"cat",
+			"date",
+			"picture",
+			"url"
+		], [
+			"id[=]" => $id
+		]);
 
-        return $datas;
-    }
+		return $datas;
+	}
 
-    public function content_update( $content )
-    {
-        $update_num = $this->update("shop_content", [
-            "title" => $content['title'],
-            "cat" => $content['cat'],
-            "date" => $content['date'],
-            "picture" => $content['picture'],
-            "url" => $content['url']
-        ], [
-            "id[=]" => $content['id']
-        ]);
+	public function content_update( $content )
+	{
+		$update_num = $this->update("shop_content", [
+			"title" => $content['title'],
+			"cat" => $content['cat'],
+			"date" => $content['date'],
+			"picture" => $content['picture'],
+			"url" => $content['url']
+		], [
+			"id[=]" => $content['id']
+		]);
 
-        return $update_num;
-    }
+		return $update_num;
+	}
 
-    public function passwd_update( $passwd )
-    {
-        $update_num = $this->update("shop_user", [
-            "password" => $passwd
-        ], [
-            "username[=]" => $_SESSION['user']
-        ]);
+	public function passwd_update( $passwd )
+	{
+		$update_num = $this->update("shop_user", [
+			"password" => $passwd
+		], [
+			"username[=]" => $_SESSION['user']
+		]);
 
-        return $update_num;
-    }
+		return $update_num;
+	}
 
-    public function cat_to_name( $catid )
-    {
-        $datas = $this->select("shop_cat", [
-            "catname"
-        ], [
-            "id[=]" => $catid
-        ]);
+	public function cat_to_name( $catid )
+	{
+		$datas = $this->select("shop_cat", [
+			"catname"
+		], [
+			"id[=]" => $catid
+		]);
 
-        return $datas[0]['catname'];
-    }
-    public function cus_list()
-    {
-        $datas = $this->select("customers", [
-            "cus_no",
-            "comp_name",
-            "cus_name",
-            "phone_no",
-            "sales_man",
-            "update_time"
-        ], [
-            "ORDER" => "cus_no"
-        ]);
+		return $datas[0]['catname'];
+	}
+	public function cus_list()
+	{
+		$datas = $this->select("customers", [
+			"cus_no",
+			"comp_name",
+			"cus_name",
+			"phone_no",
+			"sales_man",
+			"update_time"
+		], [
+			"ORDER" => "cus_no"
+		]);
 
-        return $datas;
-    }
+		return $datas;
+	}
 
-    public function get_cuss_by_no( $id )
-    {
-        $datas = $this->select("customers", [
-            "cus_no",
-            "comp_name",
-            "cus_name",
-            "phone_no",
-            "sales_man",
-            "update_time"
-        ], [
-            "cus_no[=]" => $id
-        ]);
+	public function get_cuss_by_no( $id )
+	{
+		$datas = $this->select("customers", [
+			"cus_no",
+			"comp_name",
+			"cus_name",
+			"phone_no",
+			"sales_man",
+			"update_time"
+		], [
+			"cus_no[=]" => $id
+		]);
 
-        return $datas;
-    }
-    public function cus_add( $content )
-    {
-        $last_cus_no = $this->insert("customers", [
-            "cus_no" => $content['cus_no'],
-            "comp_name" => $content['comp_name'],
-            "cus_name" => $content['cus_name'],
-            "phone_no" => $content['phone_no'],
-            "sales_man" => $content['sales_man'],
-            "update_time" => $content['update_time']
-        ]);
+		return $datas;
+	}
+		public function cus_add( $content )
+	{
+		$last_cus_no = $this->insert("customers", [
+			"cus_no" => $content['cus_no'],
+			"comp_name" => $content['comp_name'],
+			"cus_name" => $content['cus_name'],
+			"phone_no" => $content['phone_no'],
+			"sales_man" => $content['sales_man'],
+			"update_time" => $content['update_time']
+		]);
 
-        return $last_cus_no;
-    }
-    public function cuss_update( $cuss )
+		return $last_cus_no;
+	}
+	public function cuss_update( $cuss )
         {
                 $update_num = $this->update("customers", [
                         "comp_name" => $cuss['comp_name'],
@@ -203,14 +203,14 @@ class admin extends medoo
         }
 
 
-    public function cus_delete( $table , $id )
-    {
-        $delete_num = $this->delete($table, [
-            "cus_no[=]" => $id
-        ]);
+	public function cus_delete( $table , $id )
+	{
+		$delete_num = $this->delete($table, [
+			"cus_no[=]" => $id
+		]);
 
-        return $delete_num;
-    }
+		return $delete_num;
+	}
         public function sales_all()
         {
                 $datas = $this->query("SELECT sum(b.buy_price*a.sales_qty) as buy_amnt,sum(a.sales_price*a.sales_qty) as sales_amnt,(sum(a.sales_price*a.sales_qty)-sum(b.buy_price*a.sales_qty))/1.17 as net_margin,((sum(a.sales_price*a.sales_qty)-sum(b.buy_price*a.sales_qty))/1.17)/sum(a.sales_price*a.sales_qty) as net_margin_rate FROM sales a,items b WHERE a.item_no=b.item_no")->fetchALL();
@@ -219,26 +219,26 @@ class admin extends medoo
 
         }
 
-    public function sales_daily()
-    {
-        $datas = $this->query("SELECT a.sales_date as sales_date,sum(b.buy_price*a.sales_qty) as buy_amnt,sum(a.sales_price*a.sales_qty) as sales_amnt,(sum(a.sales_price*a.sales_qty)-sum(b.buy_price*a.sales_qty))/1.17 as net_margin,((sum(a.sales_price*a.sales_qty)-sum(b.buy_price*a.sales_qty))/1.17)/sum(a.sales_price*a.sales_qty) as net_margin_rate FROM sales a,items b WHERE a.item_no=b.item_no group by a.sales_date order by a.sales_date desc")->fetchALL();
+	public function sales_daily()
+	{
+		$datas = $this->query("SELECT a.sales_date as sales_date,sum(b.buy_price*a.sales_qty) as buy_amnt,sum(a.sales_price*a.sales_qty) as sales_amnt,(sum(a.sales_price*a.sales_qty)-sum(b.buy_price*a.sales_qty))/1.17 as net_margin,((sum(a.sales_price*a.sales_qty)-sum(b.buy_price*a.sales_qty))/1.17)/sum(a.sales_price*a.sales_qty) as net_margin_rate FROM sales a,items b WHERE a.item_no=b.item_no group by a.sales_date order by a.sales_date desc")->fetchALL();
 
                 return $datas;
 
-    }
-    public function ord_list()
-    {
-        $datas = $this->select("orders", [
-            "order_no",
-            "supplier_name",
-            "order_date",
-            "item_no",
-            "item_name",
-            "receive_qty",
-            "receive_price",
-            "sales_man"
-        ], [
-            "ORDER" => "order_no DESC",
+	}
+	public function ord_list()
+	{
+		$datas = $this->select("orders", [
+			"order_no",
+			"supplier_name",
+			"order_date",
+			"item_no",
+			"item_name",
+			"receive_qty",
+			"receive_price",
+			"sales_man"
+		], [
+			"ORDER" => "order_no DESC",
             "LIMIT" => 10
         ]);
         return $datas;
--- readme.md
+++ readme.md
@@ -1,4 +1,2 @@
 # Readme
 1. First note
-2. Tools update (VIM CONFIG and PLUGIN INSTASLL)
-
