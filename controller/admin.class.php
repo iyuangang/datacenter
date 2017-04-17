a81668bb41e0f3536a5263daeb30367a323e3052
iyuangang <yuangang@me.com>
2017-04-17 15:45:34 +0800

    test

--- controller/admin.class.php
+++ controller/admin.class.php
@@ -419,6 +419,14 @@ class adminController extends medoo
         header("Content-type: text/html; charset=utf-8");
     }
 
+    function sales_status()
+        {
+            $datacenter= new admin();
+            $datas['a']=__FUNCTION__;
+            $datas['salesman']=$database->sales_daily();
+            $this->display($datas);
+}
+
     function sales_man()
     {
         $database = new admin();
